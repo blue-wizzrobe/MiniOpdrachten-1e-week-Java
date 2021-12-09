2ebdb53836cb31ede0dc7bde77710028e28b3ac3
blue_wizzrobe <kevin@mangakissa.nl>
2021-12-09 15:35:44 +0100

    Im going for a holiday and taking with me:...

---
+++ Vakantie/OpVakantie.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class OpVakantie {
+    public static void main(String[] args) {
+        String sentence = "Ik ga op vakantie en neem mee...";
+        System.out.println(sentence);
+        runGame("Gezegd is: ");
+    }
+
+    static void runGame(String sentence) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        input = input.substring(0, 1).toUpperCase() + input.substring(1);
+        String inputLow = input.toLowerCase();
+        char inputFirst = inputLow.charAt(0);
+        char sentenceLast = sentence.charAt(sentence.length() - 1);
+
+        if (sentence == "Gezegd is: ") {
+            sentence += input;
+            System.out.println(sentence);
+        } else {          
+            if (inputFirst != sentenceLast) {
+                System.out.println(input + " begint niet met een '" + String.valueOf(sentenceLast).toUpperCase() + "'");
+            } else {
+                sentence += ", " + input;
+                System.out.println(sentence);
+            }
+        }
+        System.out.println("Voer het volgende item in:");
+        runGame(sentence);
+    }
+}
