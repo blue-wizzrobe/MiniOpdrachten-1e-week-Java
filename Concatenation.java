ef9d3b6f3f85245cc375dc0441ae4ed9b501c3ef
blue_wizzrobe <kevin@mangakissa.nl>
2021-12-09 09:42:04 +0100

    Concatenation added

---
+++ Concatenation.java
@@ -0,0 +1,14 @@
+public class Concatenation {
+    public static void main(String[] args) {
+        String str1 = "dop";
+        String str2 = "erwt";
+        String str3 = "en";
+        String result;
+
+        // Concateneer str1, str2 en str3 en assign aan result
+        result = str1 + str2 + str3;
+        // print string result
+        System.out.println(result);
+
+    }
+}
