860c2fa64a83a3e1bbd7d0ab49c492f340d8c030
blue_wizzrobe <kevin@mangakissa.nl>
2021-12-09 09:46:58 +0100

    added alternative concatenation

--- Concatenation.java
+++ Concatenation.java
@@ -4,11 +4,14 @@ public class Concatenation {
         String str2 = "erwt";
         String str3 = "en";
         String result;
+        String alternative;
 
         // Concateneer str1, str2 en str3 en assign aan result
         result = str1 + str2 + str3;
+        alternative = str1.concat(str2).concat(str3);
         // print string result
         System.out.println(result);
+        System.out.println(alternative);
 
     }
 }
